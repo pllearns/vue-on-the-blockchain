7725f4c4efa914414492dacf6ae57783d1ed2959
pllearns <pllearns@gmail.com>
2017-10-30 12:43:47 -0700

    To be continued: contract can be initialized, now, just need to deploy

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <button type="button" @click="generateContract()">Generate a contract</button>
+    <button type="button" @click="deployContract()">Deploy a contract</button>
   </div>
 </template>
 
@@ -9,6 +10,12 @@
 import web3 from 'web3'
 let provider = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
 let contract = require('truffle-contract')
+
+let MyContract = contract({
+  abi: '[{"constant":true,"inputs":[],"name":"getState","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getSecret","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newState","type":"string"}],"name":"setState","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"you_awesome","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"}]',
+  contractName: 'VueExample',
+  address: '0x3ef102fabe4f17119f7759a6f51db8f8835f8062'
+})
 
 export default {
   name: 'HelloWorld',
@@ -19,13 +26,15 @@ export default {
   },
   methods: {
     generateContract () {
-      let MyContract = contract({
-        abi: '',
-        unlinked_binary: '',
-        address: '0x3ef102fabe4f17119f7759a6f51db8f8835f8062'
-      })
       MyContract.setProvider(provider); // undefined!!!
       console.log('Is there a contract here? ', MyContract);
+    },
+    deployContract () {
+      MyContract.deployed().then((instance) => {
+        let deployed = instance;
+        console.log('what instance do I get? ', instance)
+        return instance
+      })
     }
   }
 }
